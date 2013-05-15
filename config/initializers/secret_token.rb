41a3c6188d4bc8e06e5d23533fded619bf4298b5
Antonio Tapiador del Dujo <atapiador@dit.upm.es>
2013-05-15 22:16:32 +0200

    Initial app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+DrTapiParty::Application.config.secret_token = '54fd59af9ba5a59e4be2ab8b485e837d6a6dedd17187eb4f6a12a99957cb73b3ccb9e09c7733810a6f77356bed1989522ffacdbf68010f0453510bccd53a7c90'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+DrTapiParty::Application.config.session_store :cookie_store, key: '_dr-tapi-party_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# DrTapiParty::Application.config.session_store :active_record_store
